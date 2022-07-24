f62f7563c9b1aa5278e64bed0c870b1e4f341785
Eugenio <carvalhoAlberto03@gmail.com>
2022-07-24 16:02:30 -0300

    feat: error handling pattern

---
+++ src/main/java/com/api/auth/exception/ResponseError.java
@@ -0,0 +1,25 @@
+package com.api.auth.exception;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class ResponseError implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+    
+    private String message;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ", timezone = "america/sao_paulo")
+    private LocalDateTime timeStamp = LocalDateTime.now();
+    private String status;
+
+    public ResponseError(String message, HttpStatus status) {
+        this.message = message;
+        this.status = status.getReasonPhrase()
+        .concat(" " + (Integer.valueOf(status.value()).toString()));
+    }
+}
